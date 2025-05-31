6de292e154fd1fa7c471fe07379490b8fd726ace
Joaopedro1717 <j.pedrominuto@hotmail.com>
2025-05-31 12:12:25 -0300

    banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'senha',
+      database: 'meubanco',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // false em produção
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
