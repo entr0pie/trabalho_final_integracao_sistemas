4cde42afa6faeeb6ecb2e6ed72c0514b1bad81c9
Joaopedro1717 <j.pedrominuto@hotmail.com>
2025-06-03 23:07:19 -0300

    redis na parte de mensagens

--- receive-send-api/src/app.module.ts
+++ receive-send-api/src/app.module.ts
@@ -8,10 +8,18 @@ import { MessageModule } from './message/message.module';
 import { AuthModule } from './auth/auth.module';
 import { QueueModule } from './queue/queue.module';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
   imports: [
     HttpModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+    }),
     ConfigModule.forRoot({ isGlobal: true}),
     TypeOrmModule.forRoot({
       type: 'postgres',
--- receive-send-api/src/message/message.service.ts
+++ receive-send-api/src/message/message.service.ts
@@ -1,13 +1,15 @@
 // src/message/message.service.ts
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
-
-
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class MessageService {
-    constructor(private readonly httpService: HttpService) {}
+    constructor(private readonly httpService: HttpService,
+      @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    ) {}
 
     async saveToDatabase(userIdSend: number, userIdReceive: number, message: string, token: string) {
         const response$ = this.httpService.post(
@@ -23,22 +25,31 @@ export class MessageService {
         await lastValueFrom(response$);
     }
 
-    async getConversation(userId1: number, userId2: number) {
-    const response$ = this.httpService.get(
-      `${process.env.RECORD_API}`,
-      {
-        params: {
-          user_id_receive: userId2,
-          page: 1,
-          size: 100,
+    async getConversation(userId1: number, userId2: number, token: string) {
+      const cacheKey = `conversation:${userId1}:${userId2}`;
+      const cached = await this.cacheManager.get(cacheKey);
+
+      if (cached) {
+        return cached;
+      }
+      const response$ = this.httpService.get(
+        `${process.env.RECORD_API}`,
+        {
+          params: {
+            user_id_receive: userId2,
+            page: 1,
+            size: 100,
+          },
+          headers: {
+            Authorization: 'Bearer ${token}',
+          },
         },
-        headers: {
-          Authorization: 'Bearer ${token}',
-        },
-      },
     );
 
     const response = await lastValueFrom(response$);
+
+    await this.cacheManager.set(cacheKey, response.data, 300); 
+
     return response.data; 
   }
 }
