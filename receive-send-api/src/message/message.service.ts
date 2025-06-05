c2efbc9a2c4f298df4f21e0cbea53aa238b18415
entr0pie <caiohporcel@gmail.com>
2025-06-05 02:56:46 -0300

    feat: ajustes na receive-send-api

--- receive-send-api/src/message/message.service.ts
+++ receive-send-api/src/message/message.service.ts
@@ -4,44 +4,49 @@ import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MessageService {
+
+    private readonly recordApiUrl: string;
+
     constructor(private readonly httpService: HttpService,
       @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    ) {}
+      private readonly config: ConfigService,
+    ) {
+      this.recordApiUrl = this.config.get<string>('RECORD_API_BASE_URL') ?? '';
 
-    async saveToDatabase(userIdSend: number, userIdReceive: number, message: string, token: string) {
+      if (!this.recordApiUrl) {
+        throw new Error('Erro: variável RECORD_API_BASE_URL não definida');
+      }
+    }
+
+    async saveToDatabase(userIdSend: number, userIdReceive: number, message: string) {
         const response$ = this.httpService.post(
-            `${process.env.RECORD_API}`,
+            this.recordApiUrl + '/messages',
             { userIdSend, userIdReceive, message },
-            { 
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            },
         );
 
         await lastValueFrom(response$);
     }
 
-    async getConversation(userId1: number, userId2: number, token: string) {
+    async getConversation(userId1: number, userId2: number) {
       const cacheKey = `conversation:${userId1}:${userId2}`;
       const cached = await this.cacheManager.get(cacheKey);
 
       if (cached) {
         return cached;
       }
+
       const response$ = this.httpService.get(
-        `${process.env.RECORD_API}`,
+        this.recordApiUrl + '/messages',
         {
           params: {
-            user_id_receive: userId2,
+            userIdSend: userId1,
+            userIdReceive: userId2,
             page: 1,
             size: 100,
-          },
-          headers: {
-            Authorization: `Bearer ${token}`,
           },
         },
     );
